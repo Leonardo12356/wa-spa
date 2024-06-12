0f806852837f5d132bc6d3f69dbf3714a97690c8
Leonardo12356 <leonardoaguiar1235@gmail.com>
2024-06-11 23:10:59 -0300

    :sparkles: ciração cards, ajustes de algumas lógicas e criação do form,lista e menu

--- src/components/dashboard/cards/CardTotalClientes.vue
+++ src/components/dashboard/cards/CardTotalClientes.vue
@@ -1,19 +1,34 @@
 <script setup>
+	import { computed, onMounted } from 'vue';
+	import { useGetTotalClientesAPI } from '../../../composables/core/api/cliente/useGetTotalClientesAPI';
 	import BaseCardItemQuantidade from './BaseCardItemQuantidade.vue';
 	import { useRouter } from 'vue-router';
+	import { useClienteStore } from '../../../store/clienteStore';
 
 	const router = useRouter();
 
+	const clienteStore = useClienteStore();
+
+	const totalClientes = computed(
+		() => clienteStore.getClientesQuantidadeTotal,
+	);
+
+	const { fetchTotalClientes } = useGetTotalClientesAPI();
+
 	const redirecionarGerenciarClientes = () => {
-		router.push({ name: 'DashboardView' });
+		router.push({ name: 'GerenciarClientesView' });
 	};
+
+	onMounted(async () => {
+		await fetchTotalClientes();
+	});
 </script>
 
 <template>
 	<base-card-item-quantidade
 		icon-card="mdi-account"
 		titulo="Clientes Ativos"
-		:quantidade-item="1000"
+		:quantidade-item="totalClientes"
 		@on-arrow-redirect="redirecionarGerenciarClientes"
 	></base-card-item-quantidade>
 </template>
